4c9b0169449c431bbc7788342634757c784079b9
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2024-03-05 16:50:21 -0500

    Implement generateRandomSequence

--- paging.py
+++ paging.py
@@ -2,7 +2,22 @@ import random
 
 
 def generateRandomSequence(k, N, n, epsilon):
-    pass
+    p = [None] * n
+    p[0 : k] = range(1, k + 1)
+    L = set(range(1, k + 1))
+
+    for i in range(k, n):
+        x = random.choice(list(L))
+        y = random.choice(list(set(range(1, N + 1)).difference(L)))
+
+        if random.random() < epsilon:
+            p[i] = x
+        else:
+            p[i] = y
+            L.remove(x)
+            L.add(y)
+    
+    return p
 
 def generate(seq):
     pass
