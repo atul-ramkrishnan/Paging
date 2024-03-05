c90cfaa87b75e69a31c1a3e8e13625c3bc48d299
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2024-03-05 16:49:49 -0500

    Create function headers

---
+++ paging.py
@@ -0,0 +1,21 @@
+import random
+
+
+def generateRandomSequence(k, N, n, epsilon):
+    pass
+
+def generate(seq):
+    pass
+
+def addNoise(hseq, gamma, omega):
+    pass
+
+def blindOracle(k, seq, hseq):
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
